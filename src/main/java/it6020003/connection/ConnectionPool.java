b8670d05f2f2d91a7bde6a9eb977864767aeed2a
noforthings <154000465+noforthings@users.noreply.github.com>
2024-01-01 20:14:50 +0700

    Remove all

--- src/main/java/it6020003/connection/ConnectionPool.java
+++
@@ -1,9 +0,0 @@
-package it6020003.connection;
-
-import java.sql.*;
-
-public interface ConnectionPool {
-	public Connection getConnection(String objectname) throws SQLException;
-	
-	public void releaseConnection(Connection con, String objectname) throws SQLException;
-}
